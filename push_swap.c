26c8f873f1235d426b5e038078a4e78834af7504
Ana Rita Dores <adores@c1r9s5.42lisboa.com>
2025-06-17 14:00:45 +0100

    Fixed

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: adores <adores@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 11:49:07 by adores            #+#    #+#             */
-/*   Updated: 2025/06/16 14:05:39 by adores           ###   ########.fr       */
+/*   Updated: 2025/06/17 11:39:09 by adores           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,12 @@ int	main(int argc, char **argv)
 		return (1);
 	if (parse_this_pls(argc, argv, &a) == 0)
 	{
-		assign_this_pls(a, ft_list_size(a));
-		size = ft_list_size(a);
 		if (!is_sorted(a))
+		{
+			assign_this_pls(a, ft_list_size(a));
+			size = ft_list_size(a);
 			do_sort(&a, &b, size);
+		}
 	}
 	free_this_pls(a);
 	if (b)
